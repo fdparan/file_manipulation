92350bb1106cc9bd4fe0116e21942caaba3f790c
fdparan <francisparan@gmail.com>
2016-04-08 15:48:07 -0400

    updated search into a one-line print using list comprehension of lines in the files that match the word

--- file_keyword_search.py
+++ file_keyword_search.py
@@ -10,12 +10,11 @@ if sys.version_info.major == 3:
 
 def search(in_file, word):
     with open(in_file) as f:
-        for i, l in enumerate(f):
-            if re.search(r"\b%s\b" % word, l):
-                print("%d: %s" % (i, l))
+        print("Searching for '{}' in {}".format(word, in_file))
+        print("".join("%d: %s" % (i, l) for i, l in enumerate(f) if re.search(r"\b%s\b" % word, l)))
 
 
-def replace(in_file, old_word, new_word, line_by_line=True):
+def replace(in_file, old_word, new_word, line_by_line=False):
     print("Replacing '{}' with '{}' in '{}'".format(old_word, new_word, in_file))
 
     pattern = r"\b%s\b"
@@ -63,10 +62,10 @@ def copy(from_file, to_file, line_by_line=False):
         out_file.close()
 
 if __name__ == "__main__":
-    file_name, keyword = "foo.txt", "foo"
-else:
-    script, file_name, keyword = sys.argv
+    # script, file_name, keyword = sys.argv
+    file_name, keyword = "ars.cfg", "ars912"
 
-# search(file_name, "yars912")
-replace(file_name, keyword, "bar")
-# copy("rsso.cfg", "samp.txt")
+    # search(file_name, keyword)
+    search(file_name, "yars912")
+    # replace(file_name, keyword, "yars912")
+    # copy("rsso.cfg", "samp.txt")
